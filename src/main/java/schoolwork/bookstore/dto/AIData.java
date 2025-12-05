f5394a846dd8beef8de0c771a0546d823a5fbe99
tabris <3146463122@qq.com>
2025-12-05 09:43:17 +0800

    修正接口文档错误与优化aiData

--- src/main/java/schoolwork/bookstore/dto/AIData.java
+++ src/main/java/schoolwork/bookstore/dto/AIData.java
@@ -6,9 +6,11 @@ import lombok.Data;
 @Data
 public class AIData {
     private Long bid;
+    private String isbn;
     private String title;
     private String author;
     private String tags;
     private String description;
+    private Integer rating;
     private Integer stock;
 }
