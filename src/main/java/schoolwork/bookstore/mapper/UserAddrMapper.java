d264155b43b11f1d8cc7967d3251f91509d91955
tabris <3146463122@qq.com>
2025-12-05 08:48:48 +0800

    12.4

--- src/main/java/schoolwork/bookstore/mapper/UserAddrMapper.java
+++ src/main/java/schoolwork/bookstore/mapper/UserAddrMapper.java
@@ -2,9 +2,14 @@ package schoolwork.bookstore.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import schoolwork.bookstore.model.UserInfo;
+
+import java.util.Map;
 
 @Mapper
 public interface UserInfoMapper extends BaseMapper<UserInfo> {
 
+    @Select("SELECT ")
+    Map<String,Object> getOrderInfo(long uid);
 }
